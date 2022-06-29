cfcb660130632b062d1d1040ccfc6cc91807e896
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-06-29 11:46:42 -0300

    Upload

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+   // Tipo dos dados
+   int height, row, column, space;
+   do
+   {
+      // Pede a altura da pirâmide para o usuário
+      height = get_int("Height: ");
+   }
+   while (height < 1 || height > 8);
+   
+       for(row = 0; row < height; row++)
+   {
+         for(space = 0; space < height - row - 1; space++)
+       {
+            printf(" ");
+       }
+            for(column = 0; column <= row; column++)
+       {
+           printf("#");
+       }
+        printf("\n");
+   }
+   
+}
+
+
