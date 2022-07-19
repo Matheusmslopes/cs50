cab125606a3ae20f1cff75ff6676628b47070f45
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-07-19 12:10:28 -0300

    Upload

---
+++ pset6/marioless/mario.py
@@ -0,0 +1,17 @@
+from cs50 import get_int
+
+# condição para gerar a pirâmide
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
+    
+# se a condição for atendida, irá começar a aninhar esses loop's
+for row in range(0, height, 1):
+    for column in range(0, height, 1):
+        if (row + column < height - 1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()        
+    
